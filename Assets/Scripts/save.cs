3b2272aaae17057a1b7c354d2e97c9c316c90e8f
Hamez2008 <jameshobbs08@icloud.com>
2026-04-13 15:58:28 +0100

    MainUpload(James)
    Everything works and should default to this if stuff breaks.

--- Assets/Scripts/save.cs
+++ Assets/Scripts/save.cs
@@ -1,18 +1,10 @@
 using UnityEngine;
 
-public class WaypointScript : MonoBehaviour
+public class save : MonoBehaviour
 {
-
-    
-    public WaypointScript NextWaypoint;
-
-    public WaypointScript waypointPointOptionA;
-    public WaypointScript waypointPointOptionB;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Debug.Log("Hello World");
         
     }
 
