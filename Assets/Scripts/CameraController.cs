d72f8d86d446a97decee77b6e989bee23d6a6573
Hamez2008 <jameshobbs08@icloud.com>
2026-04-13 10:50:41 +0100

    Waypoint and map start (James)
    Made the waypoint system and made a start on map creation. Press a or b to choose a path way. Only press a or b when you come to a cross roads otherwise game will break.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,62 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+    
+
+    public WaypointScript locationA;
+    public WaypointScript locationB;
+    public bool isTrigger = false;
+    public float CameraSpeed = 2.0f;
+    public float LerpAlpha = 0f;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This script moves the cammera between 2 designated locations.
+
+        LerpAlpha += Time.deltaTime;
+        if(locationB  != null)
+        {
+            transform.position = Vector3.Lerp(locationA.transform.position, locationB.transform.position, LerpAlpha * CameraSpeed);
+        }
+
+        if (LerpAlpha * CameraSpeed > 1)
+        {
+            if (locationA.NextWaypoint != null)
+            {
+                locationA = locationB;
+                locationB = locationB.NextWaypoint;
+                LerpAlpha = 0f;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            locationA.NextWaypoint = locationA.waypointPointOptionA;
+            locationB = locationA.waypointPointOptionA;
+            LerpAlpha = 0f;
+            Debug.Log("A");
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            locationA.NextWaypoint = locationA.waypointPointOptionB;
+            locationB = locationA.waypointPointOptionB;
+            LerpAlpha = 0f;
+            Debug.Log("D");
+        }
+    }
+
+
+   
+}
---
+++ Assets/Scripts/WaypointScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class WaypointScript : MonoBehaviour
+{
+
+    
+    public WaypointScript NextWaypoint;
+
+    public WaypointScript waypointPointOptionA;
+    public WaypointScript waypointPointOptionB;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello World");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
