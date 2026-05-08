93f7d94138759de2bd8885c7b7eae65764fdb109
theguydj <micahsidwell5@gmail.com>
2026-05-08 14:47:01 +0100

    Revert to older version
    Trying to revert to a previous version that has the waypoints.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -46,7 +46,7 @@ public class CameraController : MonoBehaviour
         if(locationB  != null)
         {
             transform.position = Vector3.Lerp(locationA.transform.position, locationB.transform.position, LerpAlpha * CameraSpeed) - (locationA.gameObject.transform.forward * Offset);
-            LookAt.transform.position = Vector3.Lerp(locationA.transform.position, locationB.transform.position, LerpAlpha * (CameraSpeed + LookAtTargetSpeed)); //+ (locationA.gameObject.transform.up * Offset);
+            LookAt.transform.position = Vector3.Lerp(locationA.transform.position, locationB.transform.position, LerpAlpha * (CameraSpeed + LookAtTargetSpeed)) + (locationA.gameObject.transform.up * Offset);
         }
 
         transform.LookAt(LookAt.transform);
@@ -90,7 +90,7 @@ public class CameraController : MonoBehaviour
     }
 
 
-    //Runs this code when a collision box is entered
+
 
    
 
