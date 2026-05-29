048ee8b8c1d871a7f23aa9c2487195f725649c7c
Hamez2008 <jameshobbs08@icloud.com>
2026-05-29 14:24:28 +0100

    ReplaceBlockersWithTrees(James)
    replaced the bloskers with trees and replaced Grey lady test with grey lady asset

--- Assets/Scripts/GreyLadyJumpScare.cs
+++ Assets/Scripts/GreyLadyJumpScare.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Threading;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -14,17 +15,21 @@ public class GreyLadyJumpScare : MonoBehaviour
 
     private Vector3 endPosition;
     private Vector3 startPosition;
-    private Vector3 offset = new Vector3(0, 0, 5);
+    public Vector3 offset = new Vector3(0, 0, 5);
 
     public float MoveDuration = 3f;
     private float elapsedTime;
 
     private bool hasTriggered = false;
 
+    public bool despawnTimer = false;
+    public int despawnTimerVal = 500;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startPosition = ObjectToMove.transform.position;
+        
     }
 
     // Update is called once per frame
@@ -39,25 +44,37 @@ public class GreyLadyJumpScare : MonoBehaviour
             float percentageComplete = elapsedTime / MoveDuration;
 
             ObjectToMove.transform.position = Vector3.Lerp(startPosition, endPosition, percentageComplete);
+
+            //Debug.Log("percentage :" + percentageComplete);
+            if (percentageComplete > 1)
+            {
+                //Debug.Log("TimerRuns");
+                despawnTimer = true;
+            }
         }
+
+        if (despawnTimer)
+        {
+            despawnTimerVal -= 1;
+            if (despawnTimerVal < 0)
+            {
+                ObjectToMove.SetActive(false);
+            }
+        }
+
+    }
+
+    
+
+    public void Despawner()
+    {
+        despawnTimer = true;
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("TriggerHit Hit");
-
-        //ObjectToMove.transform.position += new Vector3(MoveLeftAmount, 0, 0);
-        //transform.position += new Vector3.Lerp(0, 0, 0);
-
         hasTriggered = true;
-
-       // elapsedTime += Time.deltaTime;
-       // float percentageComplete = elapsedTime / MoveDuration;
-
-       // ObjectToMove.transform.position = Vector3.Lerp(startPosition, endPosition, percentageComplete);
-
-
     }
 }
 
