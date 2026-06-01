325cef20a5db036a48834ba57b3f69d925764408
Hamez2008 <jameshobbs08@icloud.com>
2026-06-01 13:44:58 +0100

    JamesFixCommit

--- Assets/Scripts/CutSceneCameraController.cs
+++ Assets/Scripts/CutSceneCameraController.cs
@@ -6,16 +6,46 @@ public class CutSceneCameraController : MonoBehaviour
 
 
     public float moveSpeed;
+
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+    
+
+    float xRotation;
+    float yRotation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Keyboard.current.aKey.wasPressedThisFrame)
+        float mouseX = Input.GetAxis("Mouse  X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxis("Mouse  Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //Rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+        
+
+
+
+
+
+
+
+        if (Keyboard.current.aKey.wasPressedThisFrame)
         {
             transform.position = transform.position + new Vector3(moveSpeed * -1, 0, 0);
         }
--- Assets/Scripts/GreyLadyJumpScare.cs
+++ Assets/Scripts/GreyLadyJumpScare.cs
@@ -51,6 +51,9 @@ public class GreyLadyJumpScare : MonoBehaviour
                 //Debug.Log("TimerRuns");
                 despawnTimer = true;
             }
+
+
+
         }
 
         if (despawnTimer)
